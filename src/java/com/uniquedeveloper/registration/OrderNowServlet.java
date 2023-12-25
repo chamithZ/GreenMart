21f3e731a6471b9746892fd0ccbb44776bcd365c
it21156960 <chamith227@gmail.com>
2023-12-25 21:36:02 +0530

    initialcommit

---
+++ src/java/com/uniquedeveloper/registration/OrderNowServlet.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.uniquedeveloper.registration;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.*;
+import java.text.SimpleDateFormat;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.uniquedeveloper.registration.DbCon;
+import com.uniquedeveloper.registration.*;
+
+@WebServlet(name = "OrderNowServlet", urlPatterns = {"/order-now"})
+public class OrderNowServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+            Date date = new Date();
+
+            User name = (User) request.getSession().getAttribute("name");
+
+            if (name != null) {
+                String productId = request.getParameter("id");
+                int productQuantity = Integer.parseInt(request.getParameter("quantity"));
+                if (productQuantity <= 0) {
+                	productQuantity = 1;
+                }
+                Order orderModel = new Order();
+                orderModel.setId(Integer.parseInt(productId));
+                orderModel.setUid(name.getId());
+                orderModel.setQunatity(productQuantity);
+                orderModel.setDate(formatter.format(date));
+
+                OrderDao orderDao = new OrderDao(DbCon.getConnection());
+                boolean result = orderDao.insertOrder(orderModel);
+                if (result) {
+                    ArrayList<Cart> cart_list = (ArrayList<Cart>) request.getSession().getAttribute("cart-list");
+                    if (cart_list != null) {
+                        for (Cart c : cart_list) {
+                            if (c.getId() == Integer.parseInt(productId)) {
+                                cart_list.remove(cart_list.indexOf(c));
+                                break;
+                            }
+                        }
+                    }
+                    response.sendRedirect("orders.jsp");
+                } else {
+                    out.println("order failed");
+                }
+            } else {
+                response.sendRedirect("index.jsp");
+            }
+
+        } catch (ClassNotFoundException|SQLException e) {
+            // TODO Auto-generated catch block
+
+		} 
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+}
